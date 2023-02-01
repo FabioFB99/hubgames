e8c39915722ca6d2ae0d5ead8e45f84998f7f03c
FabioFB99 <fabiof1999@gmail.com>
2023-02-01 17:18:59 +0100

    boton color fondo completado

---
+++ hubgames/components/ButtonColor/ButtonColor.js
@@ -0,0 +1,8 @@
+import "./ButtonColor.css";
+
+export const ButtonColor = () => `
+<button class="my-btn-color" id="myntncolor">
+<img src=${"../../public/icons/color.jpeg"} alt='color icon'/>
+<h4>Color-Mode</h4>
+</button>
+`;
---
+++ hubgames/components/Header/Header.js
@@ -0,0 +1,36 @@
+import "./Header.css";
+import { ButtonColor } from "../ButtonColor/ButtonColor";
+
+export const Header = () => `
+<h2>Made by Fabio with fun ❤</h2>
+${ButtonColor()}
+`;
+export const changeBackground = () => {
+  const themeBtn = document.querySelector("#myntncolor");
+  themeBtn.addEventListener("click", () => {
+    let numaleat = Math.floor(Math.random() * ColoresFondo.length);
+    //console.log(numaleat);
+    for (let i = 0; i < ColoresFondo.length; i++) {
+      if (numaleat == i) {
+        document.body.style.backgroundColor = ColoresFondo[i];
+        //console.log(ColoresFondo[i]);
+      }
+    }
+  });
+};
+
+const ColoresFondo = [
+  "#28262C",
+  "#998FC7",
+  "#D4C2FC",
+  "#F9F5FF",
+  "#14248A",
+  "#931621",
+  "#42D9C8",
+  "#326771",
+  "#9DB5B2",
+  "#1EFFBC",
+  "#B4E33D",
+  "#EE8434",
+  "#AE8799",
+];
